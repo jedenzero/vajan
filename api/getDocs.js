4a6293b15ac3e3097c2beefb88460f663965728c
1010 <134185805+jedenzero@users.noreply.github.com>
2024-03-31 22:00:55 +0900

    Create getDocs.js

---
+++ api/getDocs.js
@@ -0,0 +1,38 @@
+const https = require('https');
+
+module.exports = async (req, res) => {
+  const options = {
+    hostname: 'api.github.com',
+    port: 443,
+    path: '/repos/jedenzero/vajan/contents/',
+    method: 'GET',
+    headers: {
+      'Authorization': `token ${process.env.vajan_docs}`,
+      'User-Agent': 'node.js'
+    }
+  };
+
+  let data = '';
+
+  const request = https.request(options, (response) => {
+    response.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    response.on('end', () => {
+      try {
+        const parsedData = JSON.parse(data);
+        const fileNames = parsedData.map(file => file.name);
+        res.json({ fileNames });
+      } catch (error) {
+        res.status(500).json({ error: '파싱 에러' });
+      }
+    });
+  });
+
+  request.on('error', (error) => {
+    res.status(500).json({ error: error.message });
+  });
+
+  request.end();
+};
